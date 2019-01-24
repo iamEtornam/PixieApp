fd5e0aa5c168906cd1d103e9f1579280858a0f19
regnex <sunumacbright@gmail.com>
2019-01-24 11:56:07 +0000

    feat: updated Ui for ProfileScreen

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,1 +1,13 @@
 import 'package:flutter/material.dart';
+
+class ProfileScreen extends StatefulWidget {
+  @override
+  _ProfileScreenState createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
