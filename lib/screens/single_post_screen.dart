02e06c106270b28012d547ec58c895583259485a
regnex <sunumacbright@gmail.com>
2019-01-24 12:40:03 +0000

    feat: display passed data from homescreen

--- lib/screens/single_post_screen.dart
+++ lib/screens/single_post_screen.dart
@@ -1,18 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_advanced_networkimage/flutter_advanced_networkimage.dart';
+import 'package:flutter_advanced_networkimage/transition_to_image.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
 
 class SinglePostScreen extends StatefulWidget {
+  final Map<String, Object> value;
+
+  const SinglePostScreen({Key key, this.value}) : super(key: key);
+
+
   @override
   _SinglePostScreenState createState() => _SinglePostScreenState();
 }
 
 class _SinglePostScreenState extends State<SinglePostScreen> {
-  var height = 45.0;
-  var width = double.infinity;
 
   @override
   Widget build(BuildContext context) {
-    return AnimatedContainer(
-      duration: Duration(seconds: 5),
+    return Scaffold(
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        child: TransitionToImage(
+            AdvancedNetworkImage(
+                '${widget.value['backdrop_path']}',
+                loadFailedCallback: () {
+                  print('Oh, no!');
+                }, useDiskCache: true, retryLimit: 8),
+            loadingWidget: SpinKitFoldingCube(
+              color: Colors.green,
+              size: 50.0,
+            ),
+            fit: BoxFit.cover,
+            placeholder: const Icon(
+              Icons.terrain,
+              size: 100.0,
+            )
+        ),
+      ),
     );
   }
 }
