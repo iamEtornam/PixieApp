395346e95fc61a8fdff51b5a7abe54bd49f8ac5d
regnex <sunumacbright@gmail.com>
2019-01-25 17:30:49 +0000

    feat: added profile edit screen

---
+++ lib/screens/edit_profile_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class EditProfileScreen extends StatefulWidget {
+  @override
+  _EditProfileScreenState createState() => _EditProfileScreenState();
+}
+
+class _EditProfileScreenState extends State<EditProfileScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          'Edit Profile',
+          style: TextStyle(color: Colors.green),
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      floatingActionButton: FloatingActionButton.extended(
+          onPressed: () {},
+          backgroundColor: Colors.green,
+          icon: Icon(Icons.save_alt),
+          label: Text(
+            'Update',
+            style: TextStyle(color: Colors.white),
+          )),
+    );
+  }
+}
